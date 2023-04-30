547adb633e893df97d0be806ea49a7b2c8737a71
Cyrus Ngan <cyrusncy@gmail.com>
2023-04-30 23:39:48 +0800

    add packer bootstrap

--- lua/user/plugins.lua
+++ lua/user/plugins.lua
@@ -1,4 +1,16 @@
-vim.cmd.packadd('packer.nvim')
+local ensure_packer = function()
+  local fn = vim.fn
+  local install_path = fn.stdpath('data') .. '/site/pack/packer/start/packer.nvim'
+  if fn.empty(fn.glob(install_path)) > 0 then
+    fn.system({ 'git', 'clone', '--depth', '1', 'https://github.com/wbthomason/packer.nvim', install_path })
+    vim.cmd [[packadd packer.nvim]]
+    return true
+  end
+  return false
+end
+
+local packer_bootstrap = ensure_packer()
+
 
 local status_ok, packer = pcall(require, "packer")
 
@@ -128,4 +140,7 @@ return packer.startup(function(use)
   -- A fancy, configurable, notification manager for NeoVim
   -- https://github.com/rcarriga/nvim-notify
   -- use("rcarriga/nvim-notify")
+  if packer_bootstrap then
+    require('packer').sync()
+  end
 end)
