cfb4de47b9601420503f5c24353d0301fe8da505
Cyrus Ngan <cyrusncy@gmail.com>
2023-12-23 19:36:26 +0800

    add todo-comments plugins

---
+++ lua/cyrusn/plugins/todo-comments.lua
@@ -0,0 +1,12 @@
+return {
+	"folke/todo-comments.nvim",
+	dependencies = { "nvim-lua/plenary.nvim" },
+	keys = {
+		{ "<leader>st", "<cmd>TodoTelescope<cr>", desc = "TODO" },
+	},
+	opts = {
+		-- your configuration comes here
+		-- or leave it empty to use the default settings
+		-- refer to the configuration section below
+	},
+}
