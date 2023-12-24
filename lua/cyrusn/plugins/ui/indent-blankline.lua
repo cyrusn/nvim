fdb494fe0ec679d12dce9fe74578865045212259
Cyrus Ngan <cyrusncy@gmail.com>
2023-12-24 11:18:17 +0800

    clean code

--- lua/cyrusn/plugins/ui/indent-blankline.lua
+++ lua/cyrusn/plugins/ui/indent-blankline.lua
@@ -9,16 +9,9 @@ return {
 		exclude = {
 			filetypes = {
 				"help",
-				"alpha",
-				"dashboard",
-				"neo-tree",
-				"Trouble",
 				"trouble",
 				"lazy",
 				"mason",
-				"notify",
-				"toggleterm",
-				"lazyterm",
 			},
 		},
 	},
