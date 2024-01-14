bb17a959a34e376c256330c23f49ee847423f3b1
Cyrus Ngan <cyrusncy@gmail.com>
2024-01-14 18:19:25 +0800

    add feature

--- lua/cyrusn/plugins/basic/telescope.lua
+++ lua/cyrusn/plugins/basic/telescope.lua
@@ -20,6 +20,7 @@ return {
 		{ "<leader>sG", "<cmd>Telescope git_commits<CR>", desc = "Search Git Commits" },
 		{ "<leader>sg", "<cmd>Telescope git_status<CR>", desc = "Search Git Status" },
 		{ '<leader>s"', "<cmd>Telescope registers<cr>", desc = "Search Registers" },
+		{ "<leader>sc", "<cmd>Telescope commands<cr>", desc = "Search Commands" },
 		{ "<leader>sa", "<cmd>Telescope autocommands<cr>", desc = "Search Auto Commands" },
 		{ "<leader>sb", "<cmd>Telescope buffers<cr>", desc = "Search Buffers" },
 		{ "<leader>bs", "<cmd>Telescope buffers<cr>", desc = "Search Buffers" },
