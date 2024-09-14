9fcb7e889fad41c92cde09b4c5992bcfcc9a50b0
Cyrus Ngan <cyrusncy@gmail.com>
2024-09-14 08:04:46 +0800

    update keymaps

--- lua/cyrusn/plugins/diffview.lua
+++ lua/cyrusn/plugins/diffview.lua
@@ -1,4 +1,8 @@
 return {
 	"sindrets/diffview.nvim",
 	config = true,
+	keys = {
+		{ "<leader>hv", "<cmd>DiffviewOpen<cr>", desc = "Open Diffview" },
+		{ "<leader>hc", "<cmd>DiffviewOpen<cr>", desc = "Close Diffview" },
+	},
 }
