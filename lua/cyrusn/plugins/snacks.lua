37521a2c8b8f86754922cde50e95fac0d2ef4d9b
Cyrus Ngan <cyrusncy@gmail.com>
2024-12-15 09:19:26 +0800

    update modules

--- lua/cyrusn/plugins/snacks.lua
+++ lua/cyrusn/plugins/snacks.lua
@@ -5,14 +5,17 @@ return {
 	keys = {
 		{ "<leader>x", "<cmd>lua Snacks.bufdelete.delete()<cr>", desc = "Close Buffer" },
 		{ "<leader>X", "<cmd>lua Snacks.bufdelete.other()<cr>", desc = "Close Other Buffers" },
-		{ "<leader>cg", "<cmd>lua Snacks.lazygit()<cr>", desc = "Toggle lazygit" },
+		{ "<leader>hg", "<cmd>lua Snacks.lazygit()<cr>", desc = "Lazygit" },
 		{ "<leader>n", "<cmd>lua Snacks.notifier.show_history()<cr>", desc = "Show Notifier History" },
 	},
 	opts = {
 		bufdelete = { enabled = true },
-		lazygit = { enabled = true },
+		dashboard = { enabled = true },
 		indent = { enabled = true },
+		input = { enabled = true },
+		lazygit = { enabled = true },
 		notifier = { enabled = true },
+		scope = { enabled = true },
 		scroll = { enabled = true },
 	},
 }
