2736f969cfb6841eec387b9a36907245e70feaa3
Cyrus Ngan <cyrusncy@gmail.com>
2024-12-18 21:31:29 +0800

    add trouble plugin

---
+++ lua/cyrusn/plugins/trouble.lua
@@ -0,0 +1,37 @@
+return {
+	"folke/trouble.nvim",
+	opts = {}, -- for default options, refer to the configuration section for custom setup.
+	cmd = "Trouble",
+	keys = {
+		{
+			"<leader>tt",
+			"<cmd>Trouble diagnostics toggle<cr>",
+			desc = "Diagnostics",
+		},
+		{
+			"<leader>tT",
+			"<cmd>Trouble diagnostics toggle filter.buf=0<cr>",
+			desc = "Buffer Diagnostics",
+		},
+		{
+			"<leader>ts",
+			"<cmd>Trouble symbols toggle focus=false<cr>",
+			desc = "Symbols",
+		},
+		{
+			"<leader>tl",
+			"<cmd>Trouble lsp toggle focus=false win.position=right<cr>",
+			desc = "Definitions & References",
+		},
+		{
+			"<leader>tL",
+			"<cmd>Trouble loclist toggle<cr>",
+			desc = "Location List",
+		},
+		{
+			"<leader>tq",
+			"<cmd>Trouble qflist toggle<cr>",
+			desc = "Quickfix List",
+		},
+	},
+}
