dbafa6a2f369383a938bb89c0abcf745c8416ee2
Cyrus Ngan <cyrusncy@gmail.com>
2024-12-21 09:34:48 +0800

    update oil config

--- lua/cyrusn/plugins/oil.lua
+++ lua/cyrusn/plugins/oil.lua
@@ -4,7 +4,7 @@ return {
 	dependencies = { { "echasnovski/mini.icons", opts = {} } },
 	keys = {
 		{ "<leader>e", "<cmd>Oil --float<cr>", desc = "Open Oil" },
-		{ "<leader>-", "<cmd>Oil --float .<cr>", desc = "Open Oil" },
+		{ "<leader>-", "<cmd>Oil .<cr>", desc = "Open Oil in root" },
 	},
 	config = function()
 		local function parse_output(proc)
@@ -57,9 +57,22 @@ return {
 		end
 
 		local detail = false
+		-- Declare a global function to retrieve the current directory
+		function _G.get_oil_winbar()
+			local dir = require("oil").get_current_dir()
+			if dir then
+				return vim.fn.fnamemodify(dir, ":p:.")
+			else
+				-- If there is no current directory (e.g. over ssh), just show the buffer name
+				return vim.api.nvim_buf_get_name(0)
+			end
+		end
 
 		require("oil").setup({
 			delete_to_trash = true,
+			win_options = {
+				winbar = "%!v:lua.get_oil_winbar()",
+			},
 			keymaps = {
 				["l"] = "actions.select",
 				["h"] = { "actions.parent", mode = "n" },
