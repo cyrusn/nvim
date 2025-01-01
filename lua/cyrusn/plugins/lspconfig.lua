e19a9f702800f5ed4c127be0b6022c0e9cfc6b51
Cyrus Ngan <cyrusncy@gmail.com>
2025-01-01 18:21:26 +0800

    do not use lsp-zero

--- lua/cyrusn/plugins/lspconfig.lua
+++ lua/cyrusn/plugins/lspconfig.lua
@@ -1,232 +1,110 @@
 return {
 	{
-		"VonHeikemen/lsp-zero.nvim",
-	},
-	{
-		"williamboman/mason.nvim",
-		keys = { { "<leader>M", "<cmd>Mason<cr>", desc = "Mason" } },
-		config = true,
-	},
-	{
-		"hrsh7th/nvim-cmp",
-		event = "InsertEnter",
-		dependencies = {
-			"hrsh7th/cmp-buffer",
-			"hrsh7th/cmp-path",
-			"hrsh7th/cmp-cmdline",
-			"L3MON4D3/LuaSnip",
-			"saadparwaiz1/cmp_luasnip",
-			"rafamadriz/friendly-snippets",
-			"onsails/lspkind.nvim",
-		},
-		config = function()
-			local cmp = require("cmp")
-			local luasnip = require("luasnip")
-
-			require("luasnip.loaders.from_vscode").lazy_load()
-
-			cmp.setup({
-				snippet = {
-					expand = function(args)
-						require("luasnip").lsp_expand(args.body)
-					end,
-				},
-				window = {
-					completion = cmp.config.window.bordered(),
-					documentation = cmp.config.window.bordered(),
-				},
-				mapping = cmp.mapping.preset.insert({
-					["<CR>"] = cmp.mapping(function(fallback)
-						if cmp.visible() then
-							if luasnip.expandable() then
-								luasnip.expand()
-							else
-								cmp.confirm({ select = true })
-							end
-						else
-							fallback()
-						end
-					end),
-
-					["<Tab>"] = cmp.mapping(function(fallback)
-						if cmp.visible() then
-							cmp.select_next_item()
-						elseif luasnip.locally_jumpable(1) then
-							luasnip.jump(1)
-						else
-							fallback()
-						end
-					end, { "i", "s" }),
-
-					["<S-Tab>"] = cmp.mapping(function(fallback)
-						if cmp.visible() then
-							cmp.select_prev_item()
-						elseif luasnip.locally_jumpable(-1) then
-							luasnip.jump(-1)
-						else
-							fallback()
-						end
-					end, { "i", "s" }), -- scroll up and down the documentation window
-					["<C-u>"] = cmp.mapping.scroll_docs(-4),
-					["<C-d>"] = cmp.mapping.scroll_docs(4),
-				}),
-
-				sources = {
-					{ name = "nvim_lsp" },
-					{ name = "luasnip" },
-					{ name = "buffer" },
-					{ name = "path" },
-				},
-
-				formatting = {
-					format = require("lspkind").cmp_format(),
-				},
-			})
-
-			cmp.setup.cmdline({ "/", "?" }, {
-				mapping = cmp.mapping.preset.cmdline(),
-				sources = {
-					{ name = "buffer" },
-				},
-			})
-
-			cmp.setup.cmdline(":", {
-				mapping = cmp.mapping.preset.cmdline(),
-				sources = cmp.config.sources({
-					{ name = "path" },
-				}, {
-					{ name = "cmdline" },
-				}),
-				matching = { disallow_symbol_nonprefix_matching = false },
-			})
-		end,
-	},
-	{
 		"neovim/nvim-lspconfig",
 		cmd = { "LspInfo", "LspInstall", "LspStart" },
 		event = { "BufReadPre", "BufNewFile" },
 		dependencies = {
-			{ "hrsh7th/cmp-nvim-lsp" },
-			{ "williamboman/mason.nvim" },
-			{ "williamboman/mason-lspconfig.nvim" },
+			"hrsh7th/cmp-nvim-lsp",
+			{
+				"williamboman/mason.nvim",
+				keys = { { "<leader>M", "<cmd>Mason<cr>", desc = "Mason" } },
+				config = true,
+			},
+			"williamboman/mason-lspconfig.nvim",
 		},
+		init = function()
+			vim.opt.signcolumn = "yes"
+		end,
 		config = function()
-			local lsp_zero = require("lsp-zero")
+			-- Add cmp_nvim_lsp capabilities settings to lspconfig
+			-- This should be executed before you configure any language server
 
-			lsp_zero.ui({
-				float_border = "rounded",
-				sign_text = {
-					error = "✘",
-					warn = " ",
-					hint = " ",
-					info = " ",
+			vim.diagnostic.config({
+				signs = {
+					text = {
+						[vim.diagnostic.severity.ERROR] = "✘",
+						[vim.diagnostic.severity.WARN] = " ",
+						[vim.diagnostic.severity.HINT] = " ",
+						[vim.diagnostic.severity.INFO] = " ",
+					},
 				},
+			})
+
+			local lspconfig_defaults = require("lspconfig").util.default_config
+			lspconfig_defaults.capabilities = vim.tbl_deep_extend(
+				"force",
+				lspconfig_defaults.capabilities,
+				require("cmp_nvim_lsp").default_capabilities()
+			)
+
+			vim.api.nvim_create_autocmd("LspAttach", {
+				desc = "LSP actions",
+				callback = function(event)
+					local map = function(mode, l, r, desc)
+						local opts = { buffer = event.buf }
+						opts.desc = desc
+						return vim.keymap.set(mode, l, r, opts)
+					end
+
+					map("n", "<leader>ca", "<cmd>lua vim.lsp.buf.code_action()<cr>", "Code Action")
+					map("n", "<leader>cr", "<cmd>lua vim.lsp.buf.rename()<cr>", "LSP Rename")
+					map("n", "<leader>ci", "<cmd>LspInfo<cr>", "LSP Info")
+
+					-- +diagnostics
+					map("n", "<leader>cl", "<cmd>lua vim.diagnostic.open_float()<cr>", "Show Line Diagnostics")
+					map("n", "[d", "<cmd>lua vim.diagnostic.goto_prev()<cr>", "Prev Diagnostic")
+					map("n", "]d", "<cmd>lua vim.diagnostic.goto_next()<cr>", "Next Diagnostic")
+				end,
 			})
 
 			local lspconfig = require("lspconfig")
-
-			local ensure_installed = {
-				"cssls",
-				"gopls",
-				"html",
-				"lua_ls",
-				"pylsp",
-				"bashls",
-				"sqlls",
-				"ts_ls",
-			}
-
-			local servers = {
-				"clangd",
-				"cssls",
-				"emmet_ls",
-				"eslint",
-				"gopls",
-				"graphql",
-				"html",
-				"prismals",
-				"sqlls",
-				"tailwindcss",
-				"volar",
-				"yamlls",
-				lua_ls = {
-					settings = {
-						Lua = {
-							diagnostics = {
-								globals = { "vim" },
-							},
-						},
-					},
-				},
-				ts_ls = {
-					init_options = {
-						preferences = {
-							-- disable CommonJS modules warning
-							disableSuggestions = true,
-						},
-					},
-				},
-				pylsp = {
-					settings = {
-						pylsp = {
-							plugins = {
-								pycodestyle = {
-									maxLineLength = 88,
-									ignore = { "E501" },
-								},
-							},
-						},
-					},
-				},
-			}
-			local lsp_attach = function(_, bufnr)
-				lsp_zero.default_keymaps({
-					buffer = bufnr,
-					preserve_mappings = false,
-				})
-
-				local map = function(mode, l, r, desc, opts)
-					opts = opts or {}
-					opts.buffer = bufnr
-					opts.desc = desc
-					return vim.keymap.set(mode, l, r, opts)
-				end
-
-				-- +code
-				map("n", "<leader>ca", "<cmd>lua vim.lsp.buf.code_action()<cr>", "Code Action")
-				map("n", "<leader>cr", "<cmd>lua vim.lsp.buf.rename()<cr>", "LSP Rename")
-				map("n", "<leader>ci", "<cmd>LspInfo<cr>", "LSP Info")
-
-				-- +diagnostics
-				map("n", "<leader>cl", "<cmd>lua vim.diagnostic.open_float()<cr>", "Show Line Diagnostics")
-				map("n", "[d", "<cmd>lua vim.diagnostic.goto_prev()<cr>", "Prev Diagnostic")
-				map("n", "]d", "<cmd>lua vim.diagnostic.goto_next()<cr>", "Next Diagnostic")
-			end
-
-			lsp_zero.extend_lspconfig({
-				float_border = "rounded",
-				lsp_attach = lsp_attach,
-				capabilities = require("cmp_nvim_lsp").default_capabilities(),
-			})
-
-			local handlers = {}
-
-			for key, val in pairs(servers) do
-				if type(key) == "number" then
-					handlers[val] = function()
-						lspconfig[val].setup({})
-					end
-				else
-					handlers[key] = function()
-						lspconfig[key].setup(val)
-					end
-				end
-			end
+			local ensure_installed = { "lua_ls", "ts_ls", "eslint", "html", "jsonls" }
 
 			require("mason-lspconfig").setup({
 				automatic_servers_installation = true,
 				ensure_installed = ensure_installed,
-				handlers = handlers,
+				handlers = {
+					function(server_name)
+						lspconfig[server_name].setup({})
+					end,
+					html = function()
+						lspconfig["html"].setup({
+							cmd = { "vscode-html-language-server", "--stdio" },
+							filetypes = { "html", "templ" },
+							init_options = {
+								configurationSection = { "html", "css", "javascript" },
+								embeddedLanguages = {
+									css = true,
+									javascript = true,
+								},
+								provideFormatter = true,
+							},
+							root_dir = function()
+								return vim.loop.cwd()
+							end,
+						})
+					end,
+					sqlls = function()
+						lspconfig["sqlls"].setup({
+							cmd = { "sql-language-server", "up", "--method", "stdio" },
+							filetypes = { "sql", "mysql" },
+							root_dir = function()
+								return vim.loop.cwd()
+							end,
+						})
+					end,
+					lua_ls = function()
+						lspconfig["lua_ls"].setup({
+							settings = {
+								Lua = {
+									diagnostics = {
+										globals = { "vim" },
+									},
+								},
+							},
+						})
+					end,
+				},
 			})
 		end,
 	},
