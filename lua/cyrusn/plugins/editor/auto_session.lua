a4ba92f8cd62dafdb0ce2f135676ba8e5c21a0ae
Cyrus Ngan <cyrusncy@gmail.com>
2025-01-20 22:13:00 +0800

    auto-session seem more easy to use

---
+++ lua/cyrusn/plugins/editor/auto_session.lua
@@ -0,0 +1,9 @@
+return {
+	"rmagatti/auto-session",
+	lazy = false,
+	keys = {
+		{ "<leader>sw", "<cmd>Autosession search<cr>", desc = "Search Session" },
+		{ "<leader>sW", "<cmd>Autosession delete<cr>", desc = "Delete Session" },
+	},
+	config = true,
+}
