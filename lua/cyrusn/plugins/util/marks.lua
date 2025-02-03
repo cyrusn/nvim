5bdb0c0c598014815d7d58f1f2aa76155a56341a
Cyrus Ngan <cyrusncy@gmail.com>
2025-02-03 22:34:56 +0800

    add mark plugin

---
+++ lua/cyrusn/plugins/util/marks.lua
@@ -0,0 +1,34 @@
+return {
+	"chentoast/marks.nvim",
+	event = "VeryLazy",
+	opts = {},
+	config = function()
+		require("marks").setup({
+			default_mappings = false,
+			mappings = {
+				set = "m",
+				toggle = "mm",
+				next = "mf",
+				prev = "mb",
+				preview = "mp",
+				delete_buf = "mX",
+				delete = "mx",
+			},
+		})
+
+		local ok, wk = pcall(require, "which-key")
+
+		if ok then
+			wk.add({
+				{ "m", group = "marks", icon = { icon = "", color = "green" } },
+				{ "mm", name = "Mark Toggle" },
+				{ "mf", name = "Next Mark" },
+				{ "mb", name = "Prev Mark" },
+				{ "mp", name = "Preview Mark" },
+				{ "ms", name = "Set Mark" },
+				{ "mx", name = "Delete Mark" },
+				{ "mX", name = "Delete Marks" },
+			})
+		end
+	end,
+}
