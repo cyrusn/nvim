d67e741e69b444b4b6015f5c759de4ea79757524
Cyrus Ngan <cyrusncy@gmail.com>
2025-02-04 21:42:30 +0800

    better style

--- lua/cyrusn/plugins/editor/neotree.lua
+++ lua/cyrusn/plugins/editor/neotree.lua
@@ -12,13 +12,35 @@ return {
 	},
 	config = function()
 		require("neo-tree").setup({
+			hide_root_node = true, -- Hide the root node.
+			-- retain_hidden_root_indent = true, -- IF the root node is hidden, keep the indentation anyhow.
+			auto_clean_after_session_restore = true,
+			default_component_configs = {
+				git_status = {
+					symbols = {
+						-- Change type
+						added = "✚", -- or "✚", but this is redundant info if you use git_status_colors on the name
+						modified = "", -- or "", but this is redundant info if you use git_status_colors on the name
+						deleted = "✖", -- this can only be used in the git_status source
+						renamed = "󰁕", -- this can only be used in the git_status source
+						-- Status type
+						untracked = "󰼅",
+						ignored = "󰍑",
+						unstaged = "󰟙",
+						staged = "󰲕",
+						conflict = "󰼇",
+					},
+				},
+			},
+			open_files_using_relative_paths = true,
+			popup_border_style = "rounded", -- "double", "none", "rounded", "shadow", "single" or "solid"
 			window = {
 				width = 30,
 				position = "float",
 				mappings = {
 					["l"] = "open",
 					["h"] = "close_node",
-					["P"] = { "toggle_preview", config = { use_float = false, use_image_nvim = true } },
+					["P"] = { "toggle_preview", config = { use_float = true, use_image_nvim = true } },
 				},
 			},
 			filesystem = {
@@ -26,12 +48,18 @@ return {
 					enabled = true,
 					leave_dirs_open = true,
 				},
+				filtered_items = {
+					show_hidden_count = false,
+					hide_dotfiles = false,
+					visible = false,
+				},
 			},
 			buffers = {
 				follow_current_file = {
 					enabled = true,
-					leave_dirs_open = true,
+					leave_dirs_open = false,
 				},
+				show_unloaded = true,
 			},
 			git_status = {
 				follow_current_file = {
