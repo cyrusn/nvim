c86e23f6271a67fa1259d614239ae9efff24b934
Cyrus Ngan <cyrusncy@gmail.com>
2025-02-09 23:36:17 +0800

    remove search group

--- lua/cyrusn/plugins/ui/whichkey.lua
+++ lua/cyrusn/plugins/ui/whichkey.lua
@@ -22,7 +22,6 @@ return {
 		})
 		wk.add({
 			{ "<leader>c", group = "code" },
-			{ "<leader>s", group = "search" },
 			{ "<leader>h", group = "hunk", icon = { icon = "", color = "red" }, mode = { "n", "v" } },
 			{ "<leader>t", group = "trouble", icon = "T" },
 			{ "<leader>L", icon = { icon = "󰫹", color = "red" } },
