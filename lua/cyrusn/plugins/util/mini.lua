da7e97b753a5b75e826d6b4c6d67ca803dd8b33c
Cyrus Ngan <cyrusncy@gmail.com>
2025-02-27 13:45:45 +0800

    use catppuccin instead

--- lua/cyrusn/plugins/util/mini.lua
+++ lua/cyrusn/plugins/util/mini.lua
@@ -9,5 +9,6 @@ return {
 	config = function()
 		require("mini.ai").setup()
 		require("mini.bracketed").setup()
+		-- require("mini.hues").setup({ background = "#002734", foreground = "#c0c8cc", accent = "fg" }) -- azure
 	end,
 }
