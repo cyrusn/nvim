681192cdbbab26d5f20d3dc9d930a15f0c857726
Winnie <yanyi@Winnies-MacBook-Air.local>
2025-03-02 13:30:06 +0800

    update dashboard

--- lua/cyrusn/plugins/editor/snacks.lua
+++ lua/cyrusn/plugins/editor/snacks.lua
@@ -32,6 +32,40 @@ return {
 		},
 		bufdelete = {},
 		dashboard = {
+			preset = {
+				keys = {
+					{ icon = " ", key = "e", desc = "Explorer", action = ":lua Snacks.dashboard.pick('explorer')" },
+					{ icon = "󰈞", key = "f", desc = "Find File", action = ":lua Snacks.dashboard.pick('smart')" },
+					{ icon = " ", key = "n", desc = "New File", action = ":ene | startinsert" },
+					{
+						icon = " ",
+						key = "g",
+						desc = "Find Text",
+						action = ":lua Snacks.dashboard.pick('live_grep')",
+					},
+					{
+						icon = " ",
+						key = "r",
+						desc = "Recent Files",
+						action = ":lua Snacks.dashboard.pick('oldfiles')",
+					},
+					{
+						icon = " ",
+						key = "c",
+						desc = "Config",
+						action = ":lua Snacks.dashboard.pick('files', {cwd = vim.fn.stdpath('config')})",
+					},
+					{ icon = " ", key = "s", desc = "Restore Session", section = "session" },
+					{
+						icon = "󰒲 ",
+						key = "L",
+						desc = "Lazy",
+						action = ":Lazy",
+						enabled = package.loaded.lazy ~= nil,
+					},
+					{ icon = " ", key = "q", desc = "Quit", action = ":qa" },
+				},
+			},
 			sections = {
 				{ section = "header" },
 				{ icon = " ", title = "Keymaps", section = "keys", indent = 2, padding = 1 },
