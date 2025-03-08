f543351f68d9f3146822833296b37bdbe5c5ae7d
Cyrus Ngan <cyrusncy@gmail.com>
2025-03-08 19:53:14 +0800

    remove dashboard session and update explorer config

--- lua/cyrusn/plugins/editor/snacks.lua
+++ lua/cyrusn/plugins/editor/snacks.lua
@@ -31,62 +31,7 @@ return {
 			notification = {},
 		},
 		bufdelete = {},
-		dashboard = {
-			preset = {
-				header = [[
-   █████████                                          ██████   █████
-  ███░░░░░███                                        ░░██████ ░░███ 
- ███     ░░░  █████ ████ ████████  █████ ████  █████  ░███░███ ░███ 
-░███         ░░███ ░███ ░░███░░███░░███ ░███  ███░░   ░███░░███░███ 
-░███          ░███ ░███  ░███ ░░░  ░███ ░███ ░░█████  ░███ ░░██████ 
-░░███     ███ ░███ ░███  ░███      ░███ ░███  ░░░░███ ░███  ░░█████ 
- ░░█████████  ░░███████  █████     ░░████████ ██████  █████  ░░█████
-  ░░░░░░░░░    ░░░░░███ ░░░░░       ░░░░░░░░ ░░░░░░  ░░░░░    ░░░░░ 
-               ███ ░███                                             
-              ░░██████                                              
-               ░░░░░░                                               
- ]],
-				keys = {
-					{ icon = " ", key = "e", desc = "Explorer", action = ":lua Snacks.dashboard.pick('explorer')" },
-					{ icon = "󰈞", key = "f", desc = "Find File", action = ":lua Snacks.dashboard.pick('smart')" },
-					{ icon = " ", key = "n", desc = "New File", action = ":ene | startinsert" },
-					{
-						icon = " ",
-						key = "g",
-						desc = "Find Text",
-						action = ":lua Snacks.dashboard.pick('live_grep')",
-					},
-					{
-						icon = " ",
-						key = "r",
-						desc = "Recent Files",
-						action = ":lua Snacks.dashboard.pick('oldfiles')",
-					},
-					{
-						icon = " ",
-						key = "c",
-						desc = "Config",
-						action = ":lua Snacks.dashboard.pick('files', {cwd = vim.fn.stdpath('config')})",
-					},
-					{ icon = " ", key = "s", desc = "Restore Session", section = "session" },
-					{
-						icon = "󰒲 ",
-						key = "L",
-						desc = "Lazy",
-						action = ":Lazy",
-						enabled = package.loaded.lazy ~= nil,
-					},
-					{ icon = " ", key = "q", desc = "Quit", action = ":qa" },
-				},
-			},
-			sections = {
-				{ section = "header" },
-				{ icon = " ", title = "Keymaps", section = "keys", indent = 2, padding = 1 },
-				{ icon = " ", title = "Projects", section = "projects", indent = 2, padding = 1 },
-				{ section = "startup" },
-			},
-		},
-		explorer = {},
+		explorer = { replace_netrw = true },
 		indent = {},
 		lazygit = {},
 		notifier = {
