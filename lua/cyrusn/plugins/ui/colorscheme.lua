796e288c7db4610dc11d12ea694c89ef57bf9b8f
Cyrus Ngan <cyrusncy@gmail.com>
2025-03-23 14:45:55 +0800

    remove unused colorscheme

--- lua/cyrusn/plugins/ui/colorscheme.lua
+++ lua/cyrusn/plugins/ui/colorscheme.lua
@@ -1,46 +1,7 @@
 return {
 	{
-		"catppuccin/nvim",
-		enabled = true,
-		name = "catppuccin",
-		priority = 1000,
-		config = function()
-			require("catppuccin").setup({
-				flavour = "mocha", -- latte, frappe, macchiato, mocha
-				integrations = {
-					gitsigns = true,
-					mason = true,
-					mini = {
-						enabled = true,
-						indentscope_color = "",
-					},
-					fzf = true,
-					fidget = true,
-					snacks = true,
-					lsp_trouble = true,
-					navic = {
-						enabled = true,
-						custom_bg = "NONE",
-					},
-					which_key = true,
-				},
-			})
-			vim.cmd("colorscheme catppuccin")
-		end,
-	},
-	{
-		"folke/tokyonight.nvim",
-		enabled = false,
-		lazy = false,
-		priority = 1000,
-		opts = {},
-		config = function()
-			vim.cmd("colorscheme tokyonight")
-		end,
-	},
-	{
 		"rose-pine/neovim",
-		enabled = false,
+		enabled = true,
 		name = "rose-pine",
 		config = function()
 			vim.cmd("colorscheme rose-pine")
