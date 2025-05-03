6ba680c90bd4b5f46d6407f3ff2cb7cb82988371
Cyrus Ngan <cyrusncy@gmail.com>
2025-05-03 21:59:39 +0800

    add more colorscheme

--- lua/cyrusn/plugins/ui/colorscheme.lua
+++ lua/cyrusn/plugins/ui/colorscheme.lua
@@ -1,10 +1,36 @@
 return {
 	{
+		"folke/tokyonight.nvim",
+		lazy = false,
+		enabled = false,
+		priority = 1000,
+		opts = {},
+		config = function()
+			vim.cmd("colorscheme tokyonight")
+		end,
+	},
+	{
+		"catppuccin/nvim",
+		enabled = false,
+		name = "catppuccin",
+		priority = 1000,
+		config = function()
+			vim.cmd("colorscheme catppuccin")
+		end,
+	},
+	{
 		"rose-pine/neovim",
 		enabled = true,
 		name = "rose-pine",
 		config = function()
 			vim.cmd("colorscheme rose-pine")
+		end,
+	},
+	{
+		"rebelot/kanagawa.nvim",
+		enabled = false,
+		config = function()
+			vim.cmd("colorscheme kanagawa")
 		end,
 	},
 }
