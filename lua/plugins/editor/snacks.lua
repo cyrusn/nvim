a68c746157afeeb515a179c0eeb2fe750acfd8de
Cyrus Ngan <cyrusncy@gmail.com>
2026-02-17 10:04:43 +0800

    update ui

--- lua/plugins/editor/snacks.lua
+++ lua/plugins/editor/snacks.lua
@@ -7,28 +7,28 @@ return {
 		{ "<leader>bo", "<cmd>lua Snacks.bufdelete.other()<cr>", desc = "Close Other Buffers" },
 		-- lazygit
 		{ "<leader>hg", "<cmd>lua Snacks.lazygit()<cr>", desc = "Lazygit" },
-		-- notifier
-		{ "<leader>kn", "<cmd>lua Snacks.picker.notifications()<cr>", desc = "Show Notifier History" },
 		-- terminal
 		{ "<leader>uz", "<cmd>lua Snacks.terminal()<cr>", desc = "Terminal" },
 		-- explorer
 		{ "<leader>e", "<cmd>lua Snacks.explorer()<cr>", desc = "File Tree" },
 		-- picker:main
-		{ "<leader><space>", "<cmd>lua Snacks.picker.files()<cr>", desc = "Files" },
+		{ "<leader><space>", "<cmd>lua Snacks.picker.smart()<cr>", desc = "Smart" },
 		{ "<leader>,", "<cmd>lua Snacks.picker.buffers()<cr>", desc = "Buffers" },
-		{ "<leader>kp", "<cmd>lua Snacks.picker.pickers()<cr>", desc = "Pickers" },
-		{ "<leader>kg", "<cmd>lua Snacks.picker.grep()<cr>", desc = "Grep" },
-		{ "<leader>ks", "<cmd>lua Snacks.picker.smart()<cr>", desc = "Smart" },
+		-- search
+		{ "<leader>kn", "<cmd>lua Snacks.picker.notifications()<cr>", desc = "Show Notifier History" },
 		{ "<leader>kb", "<cmd>lua Snacks.picker.grep_buffers()<cr>", desc = "Grep Open Buffers" },
-		{ "<leader>kw", "<cmd>lua Snacks.picker.grep_word()<cr>", desc = "Grep Word" },
+		{ "<leader>kc", "<cmd>lua Snacks.picker.commands()<cr>", desc = "Commands" },
+		{ "<leader>kd", "<cmd>lua Snacks.picker.lsp_definitions()<cr>", desc = "LSP Definintions" },
 		{ "<leader>kf", "<cmd>lua Snacks.picker.files()<cr>", desc = "Files" },
+		{ "<leader>kg", "<cmd>lua Snacks.picker.grep()<cr>", desc = "Grep" },
 		{ "<leader>kh", "<cmd>lua Snacks.picker.help()<cr>", desc = "Help" },
 		{ "<leader>kl", "<cmd>lua Snacks.picker.lines()<cr>", desc = "Lines" },
+		{ "<leader>kp", "<cmd>lua Snacks.picker.pickers()<cr>", desc = "Pickers" },
 		{ "<leader>kq", "<cmd>lua Snacks.picker.qflist()<cr>", desc = "Quickfix" },
-		{ "<leader>kc", "<cmd>lua Snacks.picker.commands()<cr>", desc = "Commands" },
 		{ "<leader>kr", "<cmd>lua Snacks.picker.lsp_references()<cr>", desc = "LSP References" },
-		{ "<leader>kd", "<cmd>lua Snacks.picker.lsp_definitions()<cr>", desc = "LSP Definintions" },
 		{ "<leader>ku", "<cmd>lua Snacks.picker.undo()<cr>", desc = "Undo History" },
+		{ "<leader>kw", "<cmd>lua Snacks.picker.grep_word()<cr>", desc = "Grep Word" },
+		{ "<leader>kf", "<cmd>lua Snacks.picker.files()<cr>", desc = "Files" },
 		{ "<leader>k<tab>", "<cmd>lua Snacks.picker.resume()<cr>", desc = "Resume" },
 	},
 	opts = {
@@ -51,31 +51,12 @@ return {
 		picker = {
 			ui_select = { enabled = true },
 			matcher = { frecency = true },
-			layout = { preset = "custom", cycle = true, preview = true },
-			layouts = {
-				custom = {
-					layout = {
-						box = "horizontal",
-						backdrop = false,
-						row = -2,
-						width = 0.9,
-						height = 0.4,
-						border = true,
-						title = " {title} {live} {flags}",
-						{
-							box = "vertical",
-							{ win = "input", height = 1, border = "bottom" },
-							{ win = "list", border = "none" },
-						},
-						{ win = "preview", title = "{preview}", width = 0.65, border = "left" },
-					},
-				},
-			},
+			layout = { preset = "vertical", cycle = true, preview = true },
 			formatters = { file = { filename_first = true, truncate = 120 } },
 			sources = {
 				buffers = { current = false },
 				explorer = {
-					layout = { preset = "custom", preview = true },
+					layout = { preset = "vertical", preview = "main" },
 					auto_close = true,
 					git_status_open = true,
 					diagnostics_open = true,
@@ -93,7 +74,7 @@ return {
 					},
 				},
 				recent = { filter = { cwd = true } },
-				lines = { layout = { preset = "custom", preview = "main" } },
+				lines = { layout = { preset = "vertical" } },
 				lsp_type_definitions = { include_current = true, auto_confirm = false },
 				lsp_workspace_symbols = { include_current = true },
 				smart = { multi = { "buffers", "recent", "files" }, hidden = true },
