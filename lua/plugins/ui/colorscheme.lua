a8fb4735d2b19eb9881204be79c935f595d8fa42
Cyrus Ngan <cyrusncy@gmail.com>
2026-03-01 13:57:58 +0800

    use tokyonight-moon

--- lua/plugins/ui/colorscheme.lua
+++ lua/plugins/ui/colorscheme.lua
@@ -1,5 +1,15 @@
 return {
 	{
+		"folke/tokyonight.nvim",
+		-- enabled = false,
+		lazy = false,
+		priority = 1000,
+		opts = {},
+		config = function()
+			vim.cmd("colorscheme tokyonight-night")
+		end,
+	},
+	{
 		"catppuccin/nvim",
 		enabled = false,
 		name = "catppuccin",
@@ -19,7 +29,7 @@ return {
 	},
 	{
 		"craftzdog/solarized-osaka.nvim",
-		-- enabled = false,
+		enabled = false,
 		lazy = false,
 		priority = 1000,
 		config = function()
